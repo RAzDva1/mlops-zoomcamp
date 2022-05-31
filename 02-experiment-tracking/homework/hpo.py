c1a76f6a4a0c9e3ae8eea50b9df01751b59e9cf8
Валентин Горячев <vdgoryachev@sberbank.ru>
2022-05-31 12:20:59 +0300

    modify files

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -24,11 +24,14 @@ def run(data_path, num_trials):
     X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
     def objective(params):
-
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_valid)
-        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        with mlflow.start_run():
+            mlflow.set_tag("model", "RandomForestRegressor")
+            mlflow.log_params(params)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_valid)
+            rmse = mean_squared_error(y_valid, y_pred, squared=False)
+            mlflow.log_metric("rmse", rmse)
 
         return {'loss': rmse, 'status': STATUS_OK}
 
@@ -66,4 +69,4 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    run(args.data_path, args.max_evals)
+    run(args.data_path, int(args.max_evals))
