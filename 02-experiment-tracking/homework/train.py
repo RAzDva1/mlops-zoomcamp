7135d29a4d3b18cc4779be0abff38d18d066f894
Валентин Горячев <vdgoryachev@sberbank.ru>
2022-05-31 12:20:59 +0300

    modify files

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -5,6 +5,8 @@ import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+import mlflow
+
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -12,15 +14,26 @@ def load_pickle(filename: str):
 
 
 def run(data_path):
-
+    mlflow.sklearn.autolog()
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("nyc-taxi-experiment")
+    # with mlflow.start_run():
+    #     mlflow.set_tag("model", "RandomForestRegressor")
+    #     params = {
+    #         'max_depth': 10,
+    #         'random_state': 0
+    #     }
+    # mlflow.log_params(params)
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
+    # rf = RandomForestRegressor(**params)
     rf = RandomForestRegressor(max_depth=10, random_state=0)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_valid)
 
     rmse = mean_squared_error(y_valid, y_pred, squared=False)
+    # mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
